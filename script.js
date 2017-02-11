8032a1d97459b2c353334af87569c4f6777188a3
Barterr <bartgb00@gmail.com>
2017-02-11 11:31:20 -0200

    added page

---
+++ script.js
@@ -0,0 +1,27 @@
+var newQuote = function() {
+  $.ajax({
+    url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+    success: function(data) {
+      var post = data.shift(); // The data is an array of posts. Grab the first one.
+      $('#quote').html(post.content);
+      $('#author').html(post.title);
+
+      var red = Math.floor(Math.random() * 255);
+      var green = Math.floor(Math.random() * 255);
+      var blue = Math.floor(Math.random() * 255);
+      $("html body").animate({
+        backgroundColor: 'rgb('+red+','+green+','+blue+')',
+      }, 1000);
+      $("#get-quote").animate({
+        borderColor: 'rgb('+red+','+green+','+blue+')',
+      }, 1000);
+
+    },
+    cache: false
+  });
+};
+
+$(document).ready(function() {
+  newQuote();
+  $('#get-quote').on('click', newQuote);
+});
