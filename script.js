dd44885dafae474c770814791d051778f3e754d1
Barterr <bartgb00@gmail.com>
2017-02-11 15:03:43 -0200

    added tweet button

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ var newQuote = function() {
     url: '//quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
     success: function(data) {
       var post = data.shift(); // The data is an array of posts. Grab the first one.
-      var content = post.content.slice(3,post.content.length - 5);
+      var content = post.content.replace('<p>','').replace('</p>','');
+      console.log(content);
       $('#quote').html(content);
       $('#author').html("- "+post.title);
+      $('#tweet-quote').attr("href", 'https://twitter.com/intent/tweet?text=' + $('#quote').html() +"-"+post.title);
 
       var red = Math.floor(Math.random() * 255);
       var green = Math.floor(Math.random() * 255);
@@ -14,7 +16,10 @@ var newQuote = function() {
         backgroundColor: 'rgb('+red+','+green+','+blue+')',
       }, 1000);
       $("#get-quote").animate({
-        borderColor: 'rgb('+red+','+green+','+blue+')',
+        color: 'rgb('+red+','+green+','+blue+')',
+      }, 1000);
+      $("#tweet-quote").animate({
+        color: 'rgb('+red+','+green+','+blue+')',
       }, 1000);
 
     },
