8180b8ec9e1b010f646a0fa3fbf02404db807761
Barterr <bartgb00@gmail.com>
2017-02-12 21:44:37 -0200

    changed api used for quotes

--- script.js
+++ script.js
@@ -4,31 +4,49 @@
 var newQuote = function () {
   "use strict";
   $.ajax({
-    url: '//quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-    success: function (data) {
-      var post = data.shift(); // The data is an array of posts. Grab the first one.
-      var content = post.content.replace('<p>', '').replace('</p>', '');
-      console.log(content);
-      $('#quote').html(content);
-      $('#author').html("- " + post.title);
-      $('#tweet-quote').attr("href", 'https://twitter.com/intent/tweet?text=' + $('#quote').html() + "-" + post.title);
-
-      var red = Math.floor(Math.random() * 255);
-      var green = Math.floor(Math.random() * 255);
-      var blue = Math.floor(Math.random() * 255);
-      $("html body").animate({
-        backgroundColor: 'rgb(' + red + ',' + green + ',' + blue + ')'
-      }, 1000);
-      $("#get-quote").animate({
-        color: 'rgb(' + red + ',' + green + ',' + blue + ')'
-      }, 1000);
-      $("#tweet-quote").animate({
-        color: 'rgb(' + red + ',' + green + ',' + blue + ')'
-      }, 1000);
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
+    type: 'POST',
+    data: {
+      "cat": "famous"
     },
-    cache: false
+    dataType: 'json',
+    success: function(data) {
+
+      var quote = data.quote;
+      var author = data.author;
+
+      $('#quote').html(quote);
+      $('#author').html("- " + author);
+      $('#tweet-quote').attr("href", 'https://twitter.com/intent/tweet?text=' + $('#quote').html() + "-" + author);
+
+      // Randombly change background and button colors
+      newColors();
+    },
+    error: function(err) { 
+      alert(err);
+    },
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("X-Mashape-Authorization", "irRr6HqchNmshEA8zId92Jds9pAup17kfF9jsnHgOLmeGOz2NN");
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.setRequestHeader("Accept", "application/json");
+    }
   });
 };
+
+var newColors = function () {
+  var red = Math.floor(Math.random() * 255);
+  var green = Math.floor(Math.random() * 255);
+  var blue = Math.floor(Math.random() * 255);
+  $("html body").animate({
+    backgroundColor: 'rgb(' + red + ',' + green + ',' + blue + ')'
+  }, 1000);
+  $("#get-quote").animate({
+    color: 'rgb(' + red + ',' + green + ',' + blue + ')'
+  }, 1000);
+  $("#tweet-quote").animate({
+    color: 'rgb(' + red + ',' + green + ',' + blue + ')'
+  }, 1000);
+}
 
 $(document).ready(function () {
   "use strict";
